a3671d3eb651ceb98e8576a5eca1a874746fed46
Phillip Engel <phillip@goffengel.com>
2018-05-02 22:57:02 +0100

    Added controlling options when certain modules are not present. This will remove the server options if the XuleMultiProcessing.py file is not present. Remove --xule-validate if XuleValidate.py is not present. Remove --xule-compile if XuleParser is not present.
    [git-p4: depot-paths = "//ConsistencySuite/Server_Xule_Processor_DEV-Phillip/": change = 22472]

--- xule/__init__.py
+++ xule/__init__.py
@@ -24,15 +24,29 @@ limitations under the License.
 $Change$
 DOCSKIP
 """
-# from .XuleParser import parseRules
 
 from .XuleProcessor import process_xule, XuleProcessingError
-# from .XuleRuleSet import XuleRuleSet, XuleRuleSetError
 from . import XuleRuleSet as xr
 from . import XuleUtility as xu
-from . import XuleValidate as xv
 from . import XuleConstants as xc
 from .XuleContext import XuleGlobalContext, XuleRuleContext
+
+try:
+    from . import XuleValidate as xv
+except ImportError:
+    xv = None
+
+try:
+    from . import XuleParser as xp
+except ImportError:
+    xp = None
+    
+try:
+    from . import XuleMultiProcessing as xm
+except ImportError:
+    xm = None    
+
+
 from optparse import OptionParser, SUPPRESS_HELP
 from arelle import FileSource
 from arelle import ModelManager
@@ -86,12 +100,13 @@ def xuleCmdOptions(parser):
                                            "Xule Business rule")
         parser.add_option_group(parserGroup)
     
-    parserGroup.add_option("--xule-compile",
-                      action="store",
-                      dest="xule_compile",
-                      help=_("Xule files to be compiled.  "
-                             "This may be a file or directory.  When a directory is provided, all files in the directory will be processed.  "
-                             "Multiple file and directory names are separated by a '|' character. "))
+    if xp is not None: # The XuleParser is imported
+        parserGroup.add_option("--xule-compile",
+                          action="store",
+                          dest="xule_compile",
+                          help=_("Xule files to be compiled.  "
+                                 "This may be a file or directory.  When a directory is provided, all files in the directory will be processed.  "
+                                 "Multiple file and directory names are separated by a '|' character. "))
     
     parserGroup.add_option("--xule-rule-set",
                       action="store",
@@ -174,85 +189,87 @@ def xuleCmdOptions(parser):
                       dest="xule_filing_list",
                       help=_("File name of file that contains a list of filings to process"))
     
-    parserGroup.add_option("--xule-server",
-                     action="store",
-                     dest="xule_server",
-                     help=_("Launch the webserver."))
-
-    parserGroup.add_option("--xule-multi",
-                     action="store_true",
-                     dest="xule_multi",
-                     help=_("Turns on multithreading"))
+    if xm is not None:
+        parserGroup.add_option("--xule-server",
+                         action="store",
+                         dest="xule_server",
+                         help=_("Launch the webserver."))
     
-    parserGroup.add_option("--xule-cpu",
-                     action="store",
-                     dest="xule_cpu",
-                     help=_("overrides number of cpus per processing to use"))
+        parserGroup.add_option("--xule-multi",
+                         action="store_true",
+                         dest="xule_multi",
+                         help=_("Turns on multithreading"))
+        
+        parserGroup.add_option("--xule-cpu",
+                         action="store",
+                         dest="xule_cpu",
+                         help=_("overrides number of cpus per processing to use"))
+        
+        parserGroup.add_option("--xule-async",
+                         action="store_true",
+                         dest="xule_async",
+                         help=_("Outputs onscreen output as the filing is being processed"))
     
-    parserGroup.add_option("--xule-async",
-                     action="store_true",
-                     dest="xule_async",
-                     help=_("Outputs onscreen output as the filing is being processed"))
-
-    parserGroup.add_option("--xule-numthreads",
-                     action="store",
-                     dest="xule_numthreads",
-                     help=_("Indicates number of concurrents threads will run while the Xule Server is active"))
+        parserGroup.add_option("--xule-numthreads",
+                         action="store",
+                         dest="xule_numthreads",
+                         help=_("Indicates number of concurrents threads will run while the Xule Server is active"))
+        
+        parserGroup.add_option("--xule-skip",
+                          action="store",
+                          dest="xule_skip",
+                          help=_("List of rules to skip"))
+        
+        parserGroup.add_option("--xule-run-only",
+                          action="store",
+                          dest="xule_run_only",
+                          help=_("List of rules to run"))    
+        
+        parserGroup.add_option("--xule-no-cache",
+                          action="store_true",
+                          dest="xule_no_cache",
+                          help=_("Turns off local caching for a rule."))
+        
+        parserGroup.add_option("--xule-precalc-constants",
+                          action="store_true",
+                          dest="xule_precalc_constants",
+                          help=_("Pre-calculate constants that do not depend on the instance."))
     
-    parserGroup.add_option("--xule-skip",
-                      action="store",
-                      dest="xule_skip",
-                      help=_("List of rules to skip"))
+        parserGroup.add_option("--xule-exclude-nils",
+                          action="store_true",
+                          dest="xule_exclude_nils",
+                          help=_("Indicates that the processor should exclude nil facts. By default, nils are included."))
+        
+        parserGroup.add_option("--xule-include-dups",
+                          action="store_true",
+                          dest="xule_include_dups",
+                          help=_("Indicates that the processor should include duplicate facts. By default, duplicate facts are ignored."))    
+        
+        parserGroup.add_option("--xule-version",
+                          action="store_true",
+                          dest="xule_version",
+                          help=_("Display version number of the xule module."))
+        
+        parserGroup.add_option("--xule-update-rule-set-map",
+                               action="store",
+                               dest="xule_update_rule_set_map",
+                               help=_("Update the rule set map currently used. The supplied file will be merged with the current rule set map."))
     
-    parserGroup.add_option("--xule-run-only",
-                      action="store",
-                      dest="xule_run_only",
-                      help=_("List of rules to run"))    
+        parserGroup.add_option("--xule-replace-rule-set-map",
+                               action="store",
+                               dest="xule_replace_rule_set_map",
+                               help=_("Replace the rule set map currently used."))
+        
+        parserGroup.add_option("--xule-reset-rule-set-map",
+                               action="store_true",
+                               dest=("xule_reset_rule_set_map"),
+                               help=_("Reset the rule set map to the default."))
     
-    parserGroup.add_option("--xule-no-cache",
-                      action="store_true",
-                      dest="xule_no_cache",
-                      help=_("Turns off local caching for a rule."))
-    
-    parserGroup.add_option("--xule-precalc-constants",
-                      action="store_true",
-                      dest="xule_precalc_constants",
-                      help=_("Pre-calculate constants that do not depend on the instance."))
-
-    parserGroup.add_option("--xule-exclude-nils",
-                      action="store_true",
-                      dest="xule_exclude_nils",
-                      help=_("Indicates that the processor should exclude nil facts. By default, nils are included."))
-    
-    parserGroup.add_option("--xule-include-dups",
-                      action="store_true",
-                      dest="xule_include_dups",
-                      help=_("Indicates that the processor should include duplicate facts. By default, duplicate facts are ignored."))    
-    
-    parserGroup.add_option("--xule-version",
-                      action="store_true",
-                      dest="xule_version",
-                      help=_("Display version number of the xule module."))
-    
-    parserGroup.add_option("--xule-update-rule-set-map",
-                           action="store",
-                           dest="xule_update_rule_set_map",
-                           help=_("Update the rule set map currently used. The supplied file will be merged with the current rule set map."))
-
-    parserGroup.add_option("--xule-replace-rule-set-map",
-                           action="store",
-                           dest="xule_replace_rule_set_map",
-                           help=_("Replace the rule set map currently used."))
-    
-    parserGroup.add_option("--xule-reset-rule-set-map",
-                           action="store_true",
-                           dest=("xule_reset_rule_set_map"),
-                           help=_("Reset the rule set map to the default."))
-    
-    parserGroup.add_option("--xule-validate",
-                           action="store_true",
-                           dest="xule_validate",
-                           help=_("Validate ruleset"))
+    if xv is not None: # The XuleValidate module is imported
+        parserGroup.add_option("--xule-validate",
+                               action="store_true",
+                               dest="xule_validate",
+                               help=_("Validate ruleset"))
     
 def xuleCmdUtilityRun(cntlr, options, **kwargs): 
     # Save the controller and options in the module global variable
@@ -271,11 +288,16 @@ def xuleCmdUtilityRun(cntlr, options, **kwargs):
     if getattr(options, "xule_cpu", None) is not None and not getattr(options, 'xule_multi', None):
             parser.error(_("--xule-multi is required with --xule_cpu."))
 
-#     if  getattr(options, "xule_run", None) is not None and not getattr(options, 'xule_rule_set', None):
-#             parser.error(_("--xule-rule-set is required with --xule-run."))
+    if getattr(options, "xule_multi", False) and getattr(options, "xule_server", None) is None:
+        parser.error(_("--xule-multi can only be used with --xule-server enabled")) 
     
     if getattr(options, "xule_server", None) is not None and not getattr(options, 'xule_rule_set', None):
             parser.error(_("--xule-rule-set is required with --xule_server."))
+
+    from os import name
+    if getattr(options, "xule_multi", False) and name == 'nt':
+        parser.error(_("--xule-multi can't be used in Windows"))    
+
             
     if getattr(options, "xule-numthreads", None) == None:
         setattr(options, "xule-numthreads", 1)   
@@ -287,14 +309,7 @@ def xuleCmdUtilityRun(cntlr, options, **kwargs):
         parser.error(_("--xule-rule-set is required with --xule-remove-packages.")) 
 
     if getattr(options, 'xule_show_packages', None) is not None and not getattr(options, 'xule_rule_set', None):
-        parser.error(_("--xule-rule-set is required with --xule-show-packages.")) 
-
-    from os import name
-    if getattr(options, "xule_multi", False) and name == 'nt':
-        parser.error(_("--xule-multi can't be used in Windows"))    
-
-    if not getattr(options, "xule_multi", False) and getattr(options, "xule_cpu", None) is not None:
-        parser.error(_("--xule-cpu can only be used with --xule-multi enabled"))    
+        parser.error(_("--xule-rule-set is required with --xule-show-packages."))    
 
     if len([x for x in (getattr(options, "xule_update_rule_set_map", False),
                        getattr(options, "xule_replace_rule_set_map", False),
@@ -307,8 +322,7 @@ def xuleCmdUtilityRun(cntlr, options, **kwargs):
     # compile rules
     if getattr(options, "xule_compile", None):
         compile_destination = getattr(options, "xule_rule_set", "xuleRules") 
-        from .XuleParser import parseRules
-        parseRules(options.xule_compile.split("|"), compile_destination)
+        xp.parseRules(options.xule_compile.split("|"), compile_destination)
     
     # add packages
     if getattr(options, "xule_add_packages", None):
@@ -351,7 +365,6 @@ def xuleCmdUtilityRun(cntlr, options, **kwargs):
         xv.XuleValidate(cntlr, rule_set, options.xule_rule_set)
     
     if getattr(options, "xule_server", None):
-        from .XuleMultiProcessing import run_constant_group, output_message_queue
         from threading import Thread
         
         try:
@@ -361,31 +374,15 @@ def xuleCmdUtilityRun(cntlr, options, **kwargs):
             raise
 
         # Create global Context
-        global_context = XuleGlobalContext(rule_set, cntlr=cntlr, options=options  # ,
-                                           # multi=getattr(options, "xule_multi", False), 
-                                           # async=getattr(options, "xule_async", False),
-                                           # cpunum=getattr(options, "xule_cpu", None))
-                                           )
-        # global_context.show_timing = getattr(options, "xule_time", None)
-        # global_context.show_debug = getattr(options, "xule_debug", False)
-        # global_context.show_debug_table = getattr(options, "xule_debug_table", False)
-        # global_context.show_trace = getattr(options, "xule_trace", None)
-        # global_context.crash_on_error = getattr(options, "xule_crash", False)
+        global_context = XuleGlobalContext(rule_set, cntlr=cntlr, options=options)
 
         global_context.message_queue.print("Using %d processors" % (global_context.num_processors)) 
 
         # Start Output message queue
         if getattr(options, "xule_multi", False):
-            t = Thread(target=output_message_queue, args=(global_context,))
+            t = Thread(target=xm.output_message_queue, args=(global_context,))
             t.start()
         
-#         #load rules taxonomy
-#         global_context.message_queue.logging("Loading rules taxonomy")
-#         global_context.get_rules_dts()        
-# #        rules_dts = global_context.get_rules_dts()
-# #        from .XuleProcessor import load_networks
-# #        load_networks(rules_dts)
-         
         global_context.message_queue.logging("Building Constant and Rule Groups")
         global_context.all_constants = rule_set.get_grouped_constants()
         global_context.all_rules = rule_set.get_grouped_rules()        
@@ -393,18 +390,13 @@ def xuleCmdUtilityRun(cntlr, options, **kwargs):
         
         for g in global_context.all_constants:
             global_context.message_queue.logging("Constants: %s - %d" % (g, len(global_context.all_constants[g])))
-        #    for c in global_context.all_constants[g]:
-        #        print(" -- %s" % (c))
 
         for g in global_context.all_rules:
             global_context.message_queue.logging("Rules: %s - %d" % (g, len(global_context.all_rules[g])))
-            # for c in global_context.all_rules[g]:
-            #    print(" -- %s" % (c))
-
 
         # evaluate valid constants (no dependency, rules taxonomy)
         global_context.message_queue.logging("Calculating and Storing Constants")
-        run_constant_group(global_context, 'c', 'rtc')
+        xm.run_constant_group(global_context, 'c', 'rtc')
 
                                    
         # Add precalculated information to the cntlr to pass to XuleServer
@@ -469,8 +461,7 @@ def runXule(cntlr, options, modelXbrl):
             raise
 
         if getattr(options, "xule_multi", False):
-            from .XuleMultiProcessing import start_process
-            start_process(rule_set,
+            xm.start_process(rule_set,
                          modelXbrl,
                          cntlr,
                          options
