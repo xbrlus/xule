c73f9205b4507708ea39eb9ea65d3541b5dac520
Phillip Engel <phillip@goffengel.com>
2019-08-13 15:50:03 +0100

    Rewrote to render based on templates
    [git-p4: depot-paths = "//ConsistencySuite/Server_Xule_Processor_DEV-Phillip/": change = 22839]

--- FERC/render.py
+++ FERC/render.py
@@ -1,7 +1,11 @@
 from arelle import PluginManager
 from arelle.CntlrWebMain import Options
+from copy import deepcopy
 from lxml import etree
 
+import datetime
+import collections
+import json
 import logging
 import optparse
 
@@ -9,8 +13,12 @@ import optparse
 _xule_plugin_info = None
 
 # xule namespace used in the template
-_xule_namespace_map = {'xule': 'http://xbrl.us/xule/2.0/template'}
-_rule_name_prefix = 'xule-'
+_XULE_NAMESPACE_MAP = {'xule': 'http://xbrl.us/xule/2.0/template', 'xhtml': 'http://www.w3.org/1999/xhtml'}
+_XHTM_NAMESPACE = 'http://www.w3.org/1999/xhtml'
+_RULE_NAME_PREFIX = 'xule-'
+
+class FERCRenderException(Exception):
+    pass
 
 def getXulePlugin(cntlr):
     """Find the Xule plugin
@@ -49,18 +57,20 @@ def process_template(cntlr, template_file_name):
             template_tree = etree.parse(fp)
     except FileNotFoundError:
         cntlr.addToLog("Template file '{}' is not found.".format(template_file_name), 'error', level=logging.ERROR)
-        raise
+        raise FERCRenderException
 
     except etree.XMLSchemaValidateError:
         cntlr.addToLog("Template file '{}' is not a valid XHTML file.".format(template_file_name), 'error', level=logging.ERROR)
-        raise    
+        raise FERCRenderException  
 
     # build the namespace declaration for xule
     xule_namespaces = build_xule_namespaces(template_tree)
+    # build constants
+    xule_constants = build_constants()
     # create the rules for xule and retrieve the update template with the substitutions identified.
-    xule_rules, substitutions = build_xule_rules(template_tree)
+    xule_rules, substitutions = build_xule_rules(template_tree, template_file_name)
 
-    return None, template_tree
+    return '{}\n{}\n{}'.format(xule_namespaces, xule_constants, xule_rules), substitutions, template_tree
 
 def build_xule_namespaces(template_tree):
     '''build the namespace declarations for xule
@@ -71,28 +81,203 @@ def build_xule_namespaces(template_tree):
     namespaces = ['namespace {}={}'.format(k, v) if k is not None else 'namespace {}'.format(v) for k, v in template_tree.getroot().nsmap.items()]
     return '\n'.join(namespaces)
 
-def build_xule_rules(template_tree):
+def build_constants():
+    '''Create constants used by the expressions.
+
+    $currentInstant
+    $currentDuration
+    $priorInstant
+    $priorDuration
+    '''
+    constants = '''
+        constant $currentDuration = {@ferc:ReportYear}.periood
+        constant $currentInstant = $currentDuration.end
+        constant $priorDuration = $currentDuration
+        constant $priorInstant = $currentInstant
+    '''
+
+    return constants
+
+def build_xule_rules(template_tree, template_file_name):
     '''Extract the rules and identify the subsititions in the template
 
     1. Create the xule rules
     2. Identify where the subsitutions will be
     '''
     
-    substitutions = dict()
+    substitutions = collections.defaultdict(list)
     xule_rules = []
     next_rule_number = 1
+    named_rules = collections.defaultdict(list)
 
     # Go through each of the xule expressions in the template
-    for xule_frag in template_tree.findall('//xule:frag', _xule_namespace_map):
-        xule_expression = xule_frag.find('xule:expression', _xule_namespace_map)
-        if xule_expression is not None:
-            rule_name = _rule_name_prefix + str(next_rule_number)
+    for xule_expression in template_tree.findall('//xule:expression', _XULE_NAMESPACE_MAP):
+        named_rule = xule_expression.get("name")
+        if named_rule is None:
+            # This is a single rule
+            replacement_node = xule_expression.getparent()
+            if replacement_node.tag != '{{{}}}{}'.format(_XULE_NAMESPACE_MAP['xule'], 'replace'):
+                replacement_node = xule_expression
+            if replacement_node.tag != '{{{}}}{}'.format(_XULE_NAMESPACE_MAP['xule'], 'replace'):
+                replacement_node = xule_expression
+            rule_name = _RULE_NAME_PREFIX + str(next_rule_number)
             next_rule_number += 1
-            substitutions[rule_name] = (0, xule_frag)
-            rule_text = 'output {}\n{}'.format(rule_name, xule_expression.text)
+            comment_text = '    //{} - line {}'.format(template_file_name, xule_expression.sourceline)
+            if xule_expression.get('type') == 'fact':
+                substitutions[rule_name].append({'part': None, 'replacement-node': replacement_node, 'result-focus-index': 0})
+                rule_text = 'output {}\n{}\nlist((({})#rv-0).string).to-json\nrule-focus list($rv-0)'.format(rule_name, comment_text, xule_expression.text.strip())
+            else: # not a fact
+                substitutions[rule_name].append({'part': None, 'replacement-node': replacement_node, 'result-text-index': 0})
+                rule_text = 'output {}\n{}\nlist(({}).string).to-json'.format(rule_name, comment_text, xule_expression.text.strip())
             xule_rules.append(rule_text)
+        else: # This is a named rule
+            # Saved the named rule part for later
+            part = xule_expression.get('part', None)
+            named_rules[named_rule].append((part, xule_expression))
+
+    # Handle named rules
+    for named_rule, part_list in named_rules.items():
+        # Sort the part list by the part number
+        part_list.sort(key=lambda x: (x[0] is not None, x[0]) if x is not None else (False, '')) # This will put the None part first
+        result_parts = []
+        rule_focus = []
+        comments = []
+        preliminary_rule_text = ''
+
+        rule_name = _RULE_NAME_PREFIX + str(next_rule_number)
+        next_rule_number += 1
+        next_focus_number = 0
+        next_text_number = 0
+        for part, expression in part_list:
+            replacement_node = expression.getparent()
+            if replacement_node.tag != '{{{}}}{}'.format(_XULE_NAMESPACE_MAP['xule'], 'replace'):
+                replacement_node = expression
+
+            comments.append('    //{} - {}'.format(template_file_name, expression.sourceline))
+            if part is None:
+                preliminary_rule_text = expression.text.strip()
+            else:
+                if expression.get("type") == 'fact':
+                    # If the this part of the rule is a fact, then add a tag that will be used in the rule focus
+                    result_parts.append('(({})#rv-{}).string'.format(expression.text.strip(), part))
+                    rule_focus.append('$rv-{}'.format(part))
+                    substitutions[rule_name].append({'name': named_rule, 'part': part, 'replacement-node': replacement_node, 'result-focus-index': next_focus_number})
+                    next_focus_number += 1
+                else:
+                    result_parts.append('({}).string'.format(expression.text.strip()))
+                    substitutions[rule_name].append({'name': named_rule, 'part': part, 'replacement-node': replacement_node, 'result-text-index': next_text_number})
+
+                next_text_number += 1
+
+        #build rule text
+        rule_text = 'output {}\n'.format(rule_name)
+        rule_text += '\n'.join(comments) + '\n'
+        rule_text += '\n{}'.format(preliminary_rule_text)
+        result_text = ', '.join(["{}".format(expression) for expression in result_parts])
         
-    return '\n'.join(xule_rules), substitutions
+        
+        rule_text += '\nlist({}).to-json'.format(result_text)
+        if len(rule_focus) > 0:
+            focus_text = 'list({})'.format(', '.join(rule_focus))
+            rule_text += '\nrule-focus {}'.format(focus_text)
+
+        xule_rules.append(rule_text)
+
+    return '\n\n'.join(xule_rules), substitutions
+
+def substituteTemplate(substitutions, rule_results, template, modelXbrl):
+    '''Subsititute the xule expressions in the template with the generated values.'''
+
+    repeat_attribute_name = '{{{}}}{}'.format(_XULE_NAMESPACE_MAP['xule'], 'repeat')
+    repeating_nodes = {x.get(repeat_attribute_name): x for x in template.findall('//*[@xule:repeat]', _XULE_NAMESPACE_MAP)}
+
+    # Separtate the substitutions that repeat from the non repeating. This will allow the non repeating substitutions to be
+    # done first, so if a repeating node has a non repeating substitution, it will happend before the repeating node
+    # is copied.
+    non_repeating = []
+    repeating = []
+    for rule_name, subs in substitutions.items():
+        if len(subs) > 0 and subs[0].get('name') in repeating_nodes:
+            repeating.append((rule_name, subs))
+        else:
+            non_repeating.append((rule_name, subs))
+
+    # Sort the repeating so that the deepest template node substitutions are processed first. This will allow repeating within
+    # repeating.
+    def sort_by_ancestors(key):
+        subs = key[1]
+        if len(subs) == 0:
+            return 0
+        return len([x for x in subs[0]['replacement-node'].iterancestors()])
+
+    repeating.sort(key=sort_by_ancestors, reverse=True)
+    for rule_name, subs in non_repeating + repeating:
+        # Determine if this is a repeating rule.
+        new_nodes = []
+        if len(subs) > 0 and subs[0].get('name') in repeating_nodes:
+            # This is a repeating rule
+            repeating_model_node = repeating_nodes[subs[0].get('name')]
+            model_tree = etree.ElementTree(repeating_model_node)
+            
+        else:
+            repeating_model_node = None
+
+        for rule_result in rule_results[rule_name]:
+            if repeating_model_node is not None:
+                new_node = deepcopy(repeating_model_node)
+                new_nodes.append(new_node)
+                new_tree = etree.ElementTree(new_node)
+
+            for sub in subs:
+                # The value for the replacement is either text from running the rule or a fact
+                # The sub is a dictionary. If it has a result-text-index key, then the text is taken
+                # If it has a rule-focus-index key, then the value is a fact
+                if rule_name in rule_results:
+                    # Check if the result is a fact
+                    xule_expression = sub['replacement-node'].find('xule:expression', _XULE_NAMESPACE_MAP)
+                    if xule_expression is not None and xule_expression.get('type') == 'fact':
+                        fact_object_index = rule_result.refs[sub['result-focus-index']]['objectId']
+                        model_fact = modelXbrl.modelObject(fact_object_index)
+
+                        text_content, inline_format_clark = format_fact(xule_expression, model_fact)
+                    else: # Result is not a fact - use message
+                        json_result = json.loads(rule_result.msg)
+                        text_content = json_result[sub['result-text-index']]
+
+                    text_version = '<span class="sub-value sub-replacement">{}</span>'.format(text_content)
+                    
+                else:
+                    text_version = '<span class="sub-value sub-no-replacement"/>'
+
+                
+                span = etree.fromstring(text_version)
+                # Get the node in the template to replace
+                if repeating_model_node is None:
+                    sub_node = sub['replacement-node']
+                else:
+                    # Find the node in the repeating model node
+                    model_path = model_tree.getelementpath(sub['replacement-node'])
+                    sub_node = new_tree.find(model_path)
+
+                sub_parent = sub_node.getparent()
+                sub_parent.replace(sub_node, span) 
+            
+        # Add the new repeating nodes
+        for new_node in reversed(new_nodes):
+            repeating_model_node.addnext(new_node)
+        
+        # Remove the model node
+        if repeating_model_node is not None:
+            model_parent = repeating_model_node.getparent()
+            model_parent.remove(repeating_model_node)
+            
+
+    # Remove any left over xule template nodes
+    for xule_node in template.findall('//xule:*', _XULE_NAMESPACE_MAP):
+        parent = xule_node.getparent()
+        parent.remove(xule_node)
+
+    return template
 
 def fercMenuTools(cntlr, menu):
     import tkinter
@@ -119,9 +304,6 @@ def fercMenuTools(cntlr, menu):
 
     menu.add_cascade(label=_("FERC"), menu=fercMenu, underline=0)
 
-def cmdLineXbrlLoaded(cntlr, model, *Args, **kwargs):
-    pass
-
 def cmdLineOptionExtender(parser, *args, **kwargs):
     
     # extend command line options to compile rules
@@ -147,14 +329,83 @@ def fercCmdUtilityRun(cntlr, options, **kwargs):
 def cmdLineXbrlLoaded(cntlr, options, modelXbrl, *args, **kwargs):
 
     if options.ferc_render_template is not None:
-        xule_rule_text, template = process_template(cntlr, options.ferc_render_template)
+        # Process the HTML template.
+        xule_rule_text, substitutions, template = process_template(cntlr, options.ferc_render_template)
+        
+        # Compile Xule rules
+        title = template.find('//xhtml:title', _XULE_NAMESPACE_MAP)
+        if title is None:
+            cntlr.addToLog("Template does not have a title", "error")
+            raise FERCRenderException
+        xule_rule_file_name = '{}.xule'.format(title.text)
+        with open(xule_rule_file_name, 'w') as xule_file:
+            xule_file.write(xule_rule_text)
+        xule_rule_set_name = '{}.zip'.format(title.text)
+        compile_method = getXuleMethod(cntlr, 'Xule.compile')
+        compile_method(xule_rule_file_name, xule_rule_set_name, 'pickle')
+        
+        # Run Xule rules
+        # Create a log handler that will capture the messages when the rules are run.
+        log_capture_handler = _logCaptureHandler()
+        cntlr.logger.addHandler(log_capture_handler)
+
+        # Call the xule processor to run the rules
+        call_xule_method = getXuleMethod(cntlr, 'Xule.callXuleProcessor')
+        call_xule_method(cntlr, modelXbrl, xule_rule_set_name, options)
+        
+        # Remove the handler from the logger. This will stop the capture of messages
+        cntlr.logger.removeHandler(log_capture_handler)
+        
+        # Substitute template
+        rendered_template = substituteTemplate(substitutions, log_capture_handler.captured, template, modelXbrl)
+
+        # Write rendered template
+        rendered_template.write('{} - rendered.html'.format(title.text), pretty_print=True, method="xml", encoding='utf8', xml_declaration=True)
+
+
+class _logCaptureHandler(logging.Handler):
+    def __init__(self):
+        super().__init__()
+        self._captured = collections.defaultdict(list)
+
+    def emit(self, record):
+        self._captured[record.messageCode].append(record)
+    
+    @property
+    def captured(self):
+        return self._captured
+
+def format_fact(xule_expression_node, model_fact):
+    format = xule_expression_node.get('format')
+    if format is None:
+        return str(model_fact.xValue), None
+    else:
+        # convert format to clark notation 
+        if ':' in format:
+            prefix, local_name = format.split(':', 1)
+        else:
+            prefix = ''
+            local_name = format
+        ns = xule_expression_node.nsmap.get(prefix)
+        if ns is None:
+            raise FERCRenderException('Format {} is not a valid format'.format(format))
+
+        format_clark = '{{{}}}{}'.format(ns, local_name)
+        format_function = _formats.get(format_clark)
+        if format_function is None:
+            raise FERCRenderException('Format {} is not a valid format'.format(format))
+
+        return format_function(model_fact), format_clark
+
+_formats = {'{http://www.xbrl.org/inlineXBRL/transformation/2010-04-20}numcommadot': lambda mf: '{:,}'.format(mf.xValue),
+            '{http://www.xbrl.org/inlineXBRL/transformation/2010-04-20}dateslashus': lambda mf: mf.xValue.strftime('{%m/%d/%Y}')}
 
 __pluginInfo__ = {
     'name': 'FERC Tools',
     'version': '0.9',
     'description': "FERC Tools",
     'copyright': '(c) Copyright 2018 XBRL US Inc., All rights reserved.',
-    'import': 'xule'
+    'import': 'xule',
     # classes of mount points (required)
     'CntlrCmdLine.Options': cmdLineOptionExtender,
     'CntlrCmdLine.Utility.Run': fercCmdUtilityRun,
