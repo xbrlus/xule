8cf8626d82571dfa52e1a5d370f27002f11ef01a
Phillip Engel <phillip@goffengel.com>
2021-02-24 16:50:04 +0100

    Adding simple XBRL Model plugin
    [git-p4: depot-paths = "//ConsistencySuite/Server_Xule_Processor_DEV-Phillip/": change = 23193]

---
+++ SimpleXBRLModel/__init__.py
@@ -0,0 +1,29 @@
+"""__init__.py
+
+Simple XBRL odel
+
+This is the package init file.
+
+This plugin provides a simplified object model for XBRL. This model is independent of the the physical structure of the XBRL 
+that is loaded.
+
+DOCSKIP
+See https://xbrl.us/dqc-license for license information.  
+See https://xbrl.us/dqc-patent for patent infringement notice.
+Copyright (c) 2017 - 2019 XBRL US, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+$Change: 23092 $
+DOCSKIP
+"""
