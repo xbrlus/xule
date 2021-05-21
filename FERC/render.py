bff976040791e233cad00391b2af1a1628932697
Phillip Engel <phillip@goffengel.com>
2021-05-21 18:36:58 +0100

    Fix problem with -0 appearing in the rendered document.
    [git-p4: depot-paths = "//ConsistencySuite/Server_Xule_Processor_DEV-Phillip/": change = 23261]

--- FERC/render.py
+++ FERC/render.py
@@ -2611,6 +2611,13 @@ def format_numcommadot(model_fact, sign, scale, *args, **kwargs):
     sign_mult = -1 if sign == '-' else 1
     val = model_fact.xValue * sign_mult
 
+    if isinstance(val, decimal.Decimal) and val.is_zero() and val.is_signed():
+        val = decimal.Decimal(0)
+
+    # Decimals allow -0 (yes I know its weird, but its must be a math thing). If I get a -0 make it a regualr 0
+    if isinstance(val, decimal.Decimal) and val.is_zero() and val.is_signed():
+        val = decimal.Decimal(0)
+
     if scale is not None:
         # Convert scale from string to number
         try:
