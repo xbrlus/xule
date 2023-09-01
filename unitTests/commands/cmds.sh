47e058ec89e6bba2b9b6ad1499fef8bf392bee17
Campbell <campbell.pryde@xbrl.us>
2023-09-01 17:55:59 -0400

    Updated to pyparsing 3.1.1
    Seems to work ok for unit tests.

--- unitTests/commands/cmds.sh
+++ unitTests/commands/cmds.sh
@@ -14,3 +14,6 @@ python3.9 ~/arelle/Arelle-master/arellecmdline.py --plugins 'xule|transforms/SEC
 /Applications/Arelle.app/Contents/MacOS/arelleCmdLine --plugins "xule|xule/xulecat" 
 
 /Applications/Arelle.app/Contents/MacOS/arelleCmdLine --xule-compile /Users/campbellpryde/Documents/GitHub/xule/unitTests/source/base  --plugins "xule" --xule-rule-set /Users/campbellpryde/Documents/GitHub/xule/unitTests/compiled/ut-ruleset.zip --xule-max-recurse-depth=2500
+
+
+/Applications/Arelle.app/Contents/MacOS/arelleCmdLine --plugins 'xule|transforms/SEC|validate/EFM|inlineXbrlDocumentSet' --xule-run --noCertificateCheck --xule-rule-set /Users/campbellpryde/Documents/GitHub/xule/unitTests/compiled/ut-ruleset.zip --logNoRefObjectProperties --logFormat "[%(messageCode)s] %(message)s" >> /Users/campbellpryde/Documents/GitHub/xule/unitTests/output/output.txt
