484915882c7afd115dfc6079fe97a4d8d52a88f7
Campbell <campbell.pryde@xbrl.us>
2023-09-06 14:57:03 -0400

    Added xendr unit tests

---
+++ unitTests/commands/xendr_cmds.sh
@@ -0,0 +1,10 @@
+
+python3 ~/arelle/Arelle-master/arellecmdline.py --plugin 'xendr|xule' --xendr-compile --xendr-template /Users/campbellpryde/Documents/GitHub/xule/unitTests/source/xendr/Financials/110-Statement-BalanceSheet.html --xendr-template-set '/Users/campbellpryde/Documents/GitHub/xule/unitTests/compiled/xendr/Financials/110-Statement-BalanceSheet.zip' 
+
+python3 ~/arelle/Arelle-master/arellecmdline.py --plugin 'xendr|xule' --xendr-render --xendr-template-set '/Users/campbellpryde/Documents/GitHub/xule/unitTests/compiled/xendr/Financials/110-Statement-BalanceSheet.zip'  -f /Users/campbellpryde/Documents/GitHub/xule/unitTests/instance/xendr/Financials/xbrlus-20181231.htm --xendr-inline '/Users/campbellpryde/Documents/GitHub/xule/unitTests/output/xendr/Financials/110-Statement-BalanceSheet.html' --xendr-debug --xendr-show-xule-log
+
+
+python3 ~/arelle/Arelle-master/arellecmdline.py --plugin 'xendr|xule' --xendr-compile --xendr-template /Users/campbellpryde/Documents/GitHub/xule/unitTests/source/xendr/Financials/2023-Statement-BalanceSheet.html --xendr-template-set '/Users/campbellpryde/Documents/GitHub/xule/unitTests/compiled/xendr/Financials/2023-Statement-BalanceSheet.zip' 
+
+
+python3 ~/arelle/Arelle-master/arellecmdline.py --plugin 'xendr|xule' --xendr-render --xendr-template-set '/Users/campbellpryde/Documents/GitHub/xule/unitTests/compiled/xendr/Financials/2023-Statement-BalanceSheet.zip'  -f /Users/campbellpryde/Documents/GitHub/xule/unitTests/instance/xendr/Financials/adbe-20230602-597443.xml --xendr-inline '/Users/campbellpryde/Documents/GitHub/xule/unitTests/output/xendr/Financials/adbe-20230602-597443.html' --xendr-debug --xendr-show-xule-log
