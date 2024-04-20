16f66e85faa29a01996bdd6a131b216f2b6c9baa
Phillip Engel <phillip@goffengel.com>
2024-04-20 12:09:58 +0100

    Changed file type to allow keyword expansion.
    [git-p4: depot-paths = "//ConsistencySuite/Server_Xule_Processor_DEV-Phillip/": change = 23747]

--- xule/XuleInstanceFunctions.py
+++ xule/XuleInstanceFunctions.py
@@ -1,3 +1,28 @@
+"""XuleInstanceFunctions
+
+Xule is a rule processor for XBRL (X)brl r(ULE). 
+
+DOCSKIP
+See https://xbrl.us/dqc-license for license information.  
+See https://xbrl.us/dqc-patent for patent infringement notice.
+Copyright (c) 2017 - 2024 XBRL US, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+$Change$
+DOCSKIP
+"""
+
 from . import XuleValue as xv
 from .XuleRunTime import XuleProcessingError
 from .XuleModelIndexer import index_model
