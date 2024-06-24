a091d4a25a091498f4e5224ed3d345e2ef2c9a69
phillipengel <phillip@goffengel.com>
2024-06-24 14:46:12 +0200

    Realize there was that the class-subclass was only going 1 level up
    While getting the trait of the current concept, we also navigate the class-subclass relationships to find traits of the class parents. But I realized that this was only navigating to the parent and not further up ancestors. This commit  corrects this issue. #26

--- plugin/xule/XulePropertiesTrait.py
+++ plugin/xule/XulePropertiesTrait.py
@@ -11,20 +11,20 @@ def property_traits(xule_context, object_value, *args):
     
     traits = set()
     concepts = {object_value.value,} # add the concept
-    concepts |= get_parents(object_value.value, (_CLASS_SUBCLASS_2021, _CLASS_SUBCLASS_2023))
+    concepts |= get_ancestors(object_value.value, (_CLASS_SUBCLASS_2021, _CLASS_SUBCLASS_2023), False)
     for concept in concepts:
-        traits |= get_parents(concept, (_TRAIT_CONCEPT_2021, _TRAIT_CONCEPT_2023))
+        traits |= get_ancestors(concept, (_TRAIT_CONCEPT_2021, _TRAIT_CONCEPT_2023))
 
     return_values = frozenset(xv.XuleValue(xule_context, trait, 'concept') for trait in traits)
     return xv.XuleValue(xule_context, return_values, 'set', shadow_collection=frozenset(traits))
 
-def get_parents(concept, arc_roles):
+def get_ancestors(concept, arc_roles, parent_only=True):
     dts = concept.modelXbrl
     network_infos = []
     for arc_role in arc_roles:
         network_infos.extend(xp.get_base_set_info(dts, arc_role))
     
-    traits = set()
+    ancestors = set()
 
     for network_info in network_infos:
         network = dts.relationshipSet(
@@ -33,9 +33,15 @@ def get_parents(concept, arc_roles):
                 network_info[xp.NETWORK_LINK],
                 network_info[xp.NETWORK_ARC])
         for parent in  network.toModelObject(concept):
-            traits.add(parent.fromModelObject)
+            ancestors.add(parent.fromModelObject)
 
-    return traits
+    if not parent_only:
+        next_ancestors = set()
+        for ancestor in ancestors:
+            next_ancestors |= get_ancestors(ancestor, arc_roles, parent_only)
+        ancestors |= next_ancestors
+
+    return ancestors
 
 def trait_properties():
     props = {
