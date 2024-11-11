b76e381c9370327594f4b50903b65d22d746f456
phillipengel <phillip@goffengel.com>
2024-11-11 21:19:49 +0100

    Refactor XuleValue format_value method to include line number start rules

--- plugin/xendr/xendrRun.py
+++ plugin/xendr/xendrRun.py
@@ -1280,8 +1280,9 @@ def render_report(cntlr, options, modelXbrl, *args, **kwargs):
             # Gather line number start rules
             line_number_rules = set()
             for line_number_info in rule_meta_data['line-numbers'].values():
-                if 'start-rule' in line_number_info:
-                    line_number_rules.add(line_number_info['start-rule'])
+                for line_number_item in line_number_info:
+                    if 'start-rule' in line_number_item:
+                        line_number_rules.add(line_number_info['start-rule'])
 
             rule_results = run_xule_rules(cntlr, options, modelXbrl, ts, rule_meta_data['standard-rules'] + list(rule_meta_data['showifs'].keys()) + list(line_number_rules), catalog_item['xule-rule-set'])
             
