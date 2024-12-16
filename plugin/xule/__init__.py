47d05e5fc401b9f68f43878515f078d3daa1adf5
Herman Fischer <fischer@markv.com>
2024-12-15 23:09:42 -0800

    block de-registering validators for EDGAR cmd line and test suite running

--- plugin/xule/__init__.py
+++ plugin/xule/__init__.py
@@ -1059,7 +1059,7 @@ def xuleValidate(val, extra_options=None):
             if not getattr(options, "xule_run", False) and len(val.modelXbrl.facts) > 0 and len(val.modelXbrl.qnameConcepts) > 0:
                 runXule(_cntlr, options, val.modelXbrl, xule_validator['map_name'], is_validator=True)
 
-    if getattr(_cntlr, 'hasWebServer', False) and not getattr(_cntlr, 'hasGui', False):
+    if getattr(_cntlr, 'hasWebServer', False) and not getattr(_cntlr, 'hasGui', False) and not getattr(options, "block_deregister", False):
         # When arelle is running as a webserver, it will register the xule_validators on each request to the web server. 
         # The _xule_validators is emptied. On the next request, the xule_validators for that request will be re-register.
         _xule_validators = []
